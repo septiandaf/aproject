f86ce73b6d0e422ee978184afa5d58459748467c
septiandaf <septiandafajar@gmail.com>
2019-12-23 13:10:34 +0700

    penambahan jquery scroll, link active/focus, link sosmed

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,9 +30,24 @@ if($(this).scrollTop() > 10) {
 	    $(tabgroup).children('div').hide();
   		$(target).show();
 	});
-    	
+  
 
 
+// NAVIGATION MENU BAR
+$(document).ready(function(){
+	$("a[href^='#']").on("click", function(e){
+		e.preventDefault();
+
+		var target = this.hash;
+		var $target = $(target);
+
+		$("html, body").stop().animate({
+			"scrollTop": $target.offset().top
+		}, 900, "swing", function(){
+			window.location.hash = target;
+		});
+	});
+});
 
 
 
